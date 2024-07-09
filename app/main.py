02f914abdbb3a155123c6d2a6e324b2506c13e9b
dtext <textores.danny@gmail.com>
2024-07-09 23:05:53 +0200

    fix: deal with random bus numbering by trying a bunch of them

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 from fastapi import FastAPI
-from .weather import Bme280
+from .weather import get_bme280_or_exit
 
-sensor = Bme280()
+sensor = get_bme280_or_exit()
 
 app = FastAPI()
+
 
 @app.get("/")
 def get_all():
@@ -15,15 +16,18 @@ def get_all():
         "pressure": p
     }
 
+
 @app.get("/temperature")
 def get_temp():
     t, _, _ = sensor.read_sensor()
     return t
 
+
 @app.get("/pressure")
 def get_humid():
     _, p, _ = sensor.read_sensor()
     return p
+
 
 @app.get("/humidity")
 def get_pressure():
--- app/weather.py
+++ app/weather.py
@@ -1,19 +1,32 @@
 #!/usr/bin/env python3
 import smbus2
 import bme280
+import sys
+
+
+def get_bme280_or_exit():
+    for i in range(1, 13):
+        try:
+            print("trying to initialize on /dev/i2c-{}".format(i))
+            sensor = Bme280(i)
+            print("successful initialization".format(i))
+            return sensor
+        except OSError:
+            continue
+    sys.exit("could not initialize I2C bus connection with sensor")
 
 
 class Bme280:
-    def __init__(self):
-        self.port = 1
+    def __init__(self, port):
         self.address = 0x76
-        self.bus = smbus2.SMBus(self.port)
+        self.bus = smbus2.SMBus(port)
         self.calibration_params = bme280.load_calibration_params(self.bus, self.address)
-    
+
     def read_sensor(self):
         reading = bme280.sample(self.bus, self.address, self.calibration_params)
         return reading.temperature, reading.pressure, reading.humidity
-        
+
+
 if __name__ == "__main__":
     sensor = Bme280()
     t, p, h = sensor.read_sensor()
